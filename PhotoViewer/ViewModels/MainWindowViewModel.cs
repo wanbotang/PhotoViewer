d226309ef0894bc343f248952468330bda0005e3
wanbotang <279375541@qq.com>
2022-03-08 14:22:44 +0800

    Add project files.

---
+++ PhotoViewer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace PhotoViewer
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public static readonly string[] SupportedImageFileExtensions =
+        {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".bmp"
+        };
+
+        private string? _photosFolderPath;
+        private PhotoViewModel? _selectedPhoto;
+
+        public MainWindowViewModel() :
+            this(null)
+        { }
+
+        public MainWindowViewModel(string? photosFolderPath)
+        {
+            _photosFolderPath = photosFolderPath;
+            Load();
+        }
+
+        public string PhotosFolderPath
+        {
+            get => _photosFolderPath ?? Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            set
+            {
+                _photosFolderPath = value;
+                OnPropertyChanged();
+
+                // Reload the photos when the path of folder changed.
+                Load();
+            }
+        }
+
+        public PhotoViewModel? SelectedPhoto
+        {
+            get => _selectedPhoto;
+            set
+            {
+                _selectedPhoto = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<PhotoViewModel> Photos { get; } = new ObservableCollection<PhotoViewModel>();
+
+        protected void Load()
+        {
+            Photos.Clear();
+            SelectedPhoto = null;
+
+            var directory = new DirectoryInfo(PhotosFolderPath);
+            if (directory.Exists)
+            {
+                foreach (var file in directory.GetFiles())
+                {
+                    if (SupportedImageFileExtensions.Contains(file.Extension, StringComparer.OrdinalIgnoreCase))
+                    {
+                        var photo = new PhotoViewModel(new Uri(file.FullName))
+                        {
+                            Name = file.Name
+                        };
+                        Photos.Add(photo);
+                    }
+                }
+
+                if (Photos.Count > 0)
+                {
+                    SelectedPhoto = Photos[0];
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PhotoViewer/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+
+namespace PhotoViewer
+{
+    public class PhotoViewModel : INotifyPropertyChanged
+    {
+        private readonly Uri _uriSource;
+        private string? _name;
+        private BitmapImage? _thumbnail;
+
+        public PhotoViewModel(Uri uriSource)
+        {
+            _uriSource = uriSource;
+        }
+
+        public Uri UriSource => _uriSource;
+
+        public string Name
+        {
+            get => _name ?? _uriSource.ToString();
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BitmapImage Image
+        {
+            get
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = _uriSource;
+                image.EndInit();
+                image.Freeze();
+
+                return image;
+            }
+        }
+
+        public BitmapImage? Thumbnail
+        {
+            get
+            {
+                if (_thumbnail == null)
+                {
+                    _thumbnail = CreateThumbnail(_uriSource);
+                }
+
+                return _thumbnail;
+            }
+        }
+
+        public static BitmapImage CreateThumbnail(Uri uriSource, int pixelHeight = 64)
+        {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.DelayCreation;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = uriSource;
+            image.DecodePixelHeight = pixelHeight;
+            image.EndInit();
+            image.Freeze();
+
+            return image;
+        }
+
+        public override string ToString() => Name;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
